47aacd00acb8bf78647d5176f17394ef65ca12c0
Tobias <tobwiens@gmail.com>
2020-06-23 22:18:21 +0200

    Privacy notice and lean website

---
+++ _includes/gatag.js
@@ -0,0 +1,8 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-58897325-8');
+</script>
